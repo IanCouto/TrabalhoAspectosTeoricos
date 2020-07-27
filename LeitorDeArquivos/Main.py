738125bfe78d237a3042f4ea03231247de6e9488
IanCouto <48738352+IanCouto@users.noreply.github.com>
2020-07-27 15:59:54 -0300

    inicia verificação de frase

--- LeitorDeArquivos/Main.py
+++ LeitorDeArquivos/Main.py
@@ -1,6 +1,29 @@
 #!/usr/bin/python
 
 file = open('../input/input.txt', 'r')
-read_data = file.read()
-print(read_data)
+lista_spaco = [i for j in file.read().split() for i in (j, ' ')][:-1]
+print(lista_spaco)
+comentario = False
+cont = 0
+saida = []
+for str in lista_spaco:
+
+    if comentario == False:
+        print(cont)
+        if '/*' in str:
+              comentario = True
+              saida.append("COMMENT")
+        elif str in " ":
+            saida.append("SPACE")
+        elif str.isdigit():
+            saida.append("INT")
+        elif str in '=':
+            saida.append("EQUALS")
+
+    elif "*/" in str:
+        comentario = False
+
+    cont += 1
+
+print(saida)
 file.close()
